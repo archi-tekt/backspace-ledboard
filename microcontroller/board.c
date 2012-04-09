186da8a99c4c59d3dc53c8b161497054d6956b04
Fabian Mewes <architekt@coding4coffee.org>
2012-04-09 01:40:53 +0000

    add timer interrupt code

--- microcontroller/board.c
+++ microcontroller/board.c
@@ -6,50 +6,58 @@ void init ()
 {
 	/* disable interrupts */
 	cli();
-	
-	
+
+
 	/* set outputs */
 	/* LINE_A */
 	DDRC |= 1;
-	
+
 	/* LINE_B */
 	DDRC |= 1 << 1;
-	
+
 	/* LINE C */
 	DDRC |= 1 << 2;
-	
+
 	/* LINE_D */
 	DDRC |= 1 << 3;
-	
+
 	/* HE - output enable */
 	DDRC |= 1 << 4;
-	
+
 	/* RCK - TODO */
 	DDRC |= 1 << 5;
-	
+
 	/* R0 - bits */
 	DDRD |= 1 << 4;
-	
+
 	/* SCK */
 	DDRB |= 1 << 1;
-	
+
 	/* setup timer 0 */
 	/* prescaler 256 */
 	TCCR0B = 4;
-	
+
 	/* irq directly after */
 	TCNT0 = 0xFF;
-	
+
 	TIMSK0 = 1;
+
+	sei();
 }
 
 int main()
 {
+	int i;
+
 	/* init hw */
 	init();
 
 	/* remaining stuff is irq driven */
 	while (1);
+
+	for (i = 0; i < 10; ++i) {
+		asm volatile("nop");
+	}
 
 	return 0;
 }
--- microcontroller/led_array.c
+++ microcontroller/led_array.c
@@ -32,15 +32,76 @@ void led_array_data_toggle(uint8_t x, uint8_t y, uint8_t data)
 
 }
 
-ISR(TIMER0_OVF_vect)
+ISR (TIMER0_OVF_vect)
 {
-	uint8_t x, y;
+        unsigned char i = 0, t;
+        unsigned short shift;
+        //cli();
 
-	for (y = 0; y < 16; y++) {
-		for (x = 0; x < 96; x++) {
+        TIMSK0 = 0;
+        //sei();
 
-		}
-	}
+        TCNT0 = 0xd5;
 
+        /* enable output ?*/
+        clear ( PORTC, 4 );
+
+        /* set line 0 */
+        PORTC = (PORTC & 0xF0);
+
+        for ( i = 0; i < 16; i++ )
+        {
+                shift = 1 << i;
+                for ( t = 0; t < 96; t++ )
+                {
+                        /* clock low */
+                        clear ( PORTB, 1 );
+#if 0
+                        /* update bits */
+                        if ( frame_buffer[t] & shift )
+                                clear ( PORTD, 4 );
+                        else
+                                set ( PORTD, 4 );
+#endif
+			set ( PORTD, 4 );
+                        /* clock high */
+                        set ( PORTB, 1 );
+
+                }
+
+                /* disable output */
+                set( PORTC, 4 );
+
+                /* let the leds bright for some time */
+                for ( t = 0; t < 10; t++ )
+                        asm volatile ( "nop" );
+
+                /* change line */
+                PORTC = (PORTC & 0xF0) | i;
+
+                /* save? str */
+                toggle( PORTC, 5 );
+                toggle( PORTC, 5 );
+
+                /* enable output */
+                clear ( PORTC, 4 );
+                for ( t = 0; t < 10; t++ )
+                        asm volatile ( "nop" );
+        }
+
+        /* change buffers */
+        if ( update_request )
+        {
+                update_request = 0;
+                for ( i = 0; i < 96; i++ )
+                        frame_buffer[i] = frame[i];
+        }
+
+        /* disable output */
+        set ( PORTC, 4 );
+        for ( t = 0; t < 10; t++ )
+                asm volatile ( "nop" );
+        TIMSK0 = 1;
+        //sei();
 
 }
